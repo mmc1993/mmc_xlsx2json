b89a31a312818665984e1a7f60912705b6a7ee80
xinyule <xinyule@txcombo.com>
2024-08-01 19:54:08 +0800

    优化

--- Example/out/config.cs
+++ Example/out/config.cs
@@ -2,28 +2,28 @@ using System.Collections.Generic;
 
 namespace config {
     public struct Sheet1 {
-        public struct __1837178933264__ {
+        public struct __2314732556304__ {
             public bool b;
             public float n;
             public string s;
         };
-        public struct __1837170713680__ {
+        public struct __2314724336720__ {
             public bool b;
             public float n;
             public string s;
         };
-        public struct __1837178963088__ {
+        public struct __2314732586128__ {
             public bool b;
             public float n;
             public string s;
         };
-        public struct __1837178977872__ {
+        public struct __2314732600912__ {
             public bool b;
             public float n;
             public string s;
         };
-        public struct __1837178962256__ {
-            public __1837178977872__ t;
+        public struct __2314732585296__ {
+            public __2314732600912__ t;
         };
         public float id;
         public bool b;
@@ -35,10 +35,10 @@ namespace config {
         public Dictionary<string, bool> b_dict;
         public Dictionary<string, float> n_dict;
         public Dictionary<string, string> s_dict;
-        public __1837178933264__ t;
-        public List<__1837170713680__> t_list;
+        public __2314732556304__ t;
+        public List<__2314724336720__> t_list;
         public List<Dictionary<string, float>> d_list;
-        public Dictionary<string, __1837178963088__> t_dict;
-        public __1837178962256__ t_type;
+        public Dictionary<string, __2314732586128__> t_dict;
+        public __2314732585296__ t_type;
     };
 }
