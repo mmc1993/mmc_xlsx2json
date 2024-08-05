ebafae699b2a83aa355a06589fc19ddf24f5f149
xinyule <xinyule@txcombo.com>
2024-08-05 16:32:51 +0800

    优化

--- Example/out/config.cpp
+++ Example/out/config.cpp
@@ -10,28 +10,4 @@ namespace config {
         bool c;
         std::string d;
     };
-    struct demo1 {
-        struct __3147673060304__ {
-            int vint;
-            std::string vstr;
-            float vfloat;
-            bool vbool;
-        };
-        struct __3147673084048__ {
-            int a;
-            float b;
-        };
-        int a;
-        std::vector<int> b;
-        std::map<std::string, float> c;
-        __3147673060304__ d;
-        std::vector<__3147673084048__> e;
-    };
-    struct demo2 {
-
-        int a;
-        float b;
-        bool c;
-        std::string d;
-    };
 }
--- Example/out/config.cs
+++ Example/out/config.cs
@@ -8,28 +8,4 @@ namespace config {
         public bool c;
         public string d;
     };
-    public class demo1 {
-        public class __3147673060304__ {
-            public int vint;
-            public string vstr;
-            public float vfloat;
-            public bool vbool;
-        };
-        public class __3147673084048__ {
-            public int a;
-            public float b;
-        };
-        public int a;
-        public List<int> b;
-        public Dictionary<string, float> c;
-        public __3147673060304__ d;
-        public List<__3147673084048__> e;
-    };
-    public class demo2 {
-
-        public int a;
-        public float b;
-        public bool c;
-        public string d;
-    };
 }
